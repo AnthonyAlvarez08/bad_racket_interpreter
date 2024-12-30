97a19c066ed532adfb5a075ce9789fb228b2db49
wataflai <=>
2024-12-30 16:24:03 -0600

    did a little more set up of the expression evaluation loop

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,138 @@
-use std::collections::HashMap;
 
-
-// static mut FunctionTable : HashMap<String, >
+/*
+* TODO: finish parse args function
+*/
 
 fn main() {
     println!("Hello, world!");
 
-    let prog = String::from("(* (+ (* 4 3) 5) 3)");
-    let res = parsing::expression_order(&prog);
+    // let prog = String::from("(* (+ (* 4 3) 5) 3)");
+    // let res = parsing::expression_order(&prog);
 
-    for i in res {
-        println!("parentheses at {} {}", i[0], i[1]);
+    // for i in res {
+    //     println!("parentheses at {} {}", i[0], i[1]);
 
-        println!("Matching parentheses for {} is at {}", i[0], parsing::find_matching_parenthesis(&prog, i[0]))
-    }
+    //     println!("Matching parentheses for {} is at {}", i[0], parsing::find_matching_parenthesis(&prog, i[0]))
+    // }
+
+    let prog2 = "#lang racket
+
+(+ 5 4)
+(* 7 3)
+(/ (* 3 5) 5)
+(+ (* 4 3) 5)
+(* (+ (* 4 3) 5) 3)";
+
+    evaluation::evaluate(&String::from(prog2));
+
+    
 
 }
 
 mod evaluation {
+    use crate::parsing;
+
+    // global variables because I don't like having raw literals
+    const OPEN_EXPR : char = '(';
+    const CLOSE_EXPR : char = ')';
     const ARITHMETIC : [&str; 4] = ["*", "+", "-", "+"];
-    const BOOL : [&str; 10] = ["=", ">", "<", "<=", ">=", "and", "or", "xor", "nand", "nor"];
-    const CONTROL_FLOW : [&str; 3] = ["if", "cond", "else"];
+    const BOOLEAN : [&str; 10] = ["=", ">", "<", "<=", ">=", "and", "or", "xor", "nand", "nor"];
+    const CONDS : [&str; 3] = ["if", "cond", "else"];
+
+    /// Basically evaluates a whole program
+    /// (currently only supports arithmetic lol)
+    pub fn evaluate(prog: &String) {
+        let end = prog.len();
+        let mut cursor : usize = 0;
+
+        while cursor < end {
+            // go up to next opening parenthesis
+            while prog.chars().nth(cursor).unwrap_or_default() != OPEN_EXPR {
+                cursor += 1;
+
+                if cursor >= end {
+                    return;
+                }
+            }
+            // find where the current expression ends
+            let ender = parsing::find_matching_parenthesis(&prog, cursor);
+
+            // do not include the parenthesis inside the expression lol
+            let expression_substr = &prog[cursor+1..ender];
 
 
-    pub fn evaluate(prog: String) {
+            // print the result
+            println!("{}", evaluate_expresion(&String::from(expression_substr)));
+            
+            // go to next expression
+            cursor = ender - 1;
+
+        }
+
+
+    }
+
+    /// calculates the result of an individual expression
+    fn evaluate_expresion(expr: &String) -> String {
+        // get the index of the next space
+        if let Some(dex) = expr.chars().position(|x| x == ' ') {
+
+            // basically just parse the command and go to the more specific evaluation function
+
+            let command = &expr[..dex];
+
+            if ARITHMETIC.contains(&command) {
+                return eval_arithmetic(command, &expr);
+            }
+            
+            if BOOLEAN.contains(&command) {
+                return eval_boolean(command, &expr);
+            }
+
+            if CONDS.contains(&command) {
+                return eval_conditionals(&expr);
+            }
+
+            // otherwise just return this command I gues
+            panic!("Invalid symbol `{}`", command);
+        } else {
+            panic!("Invalid syntax");
+        }
+        
         
     }
 
-    fn evaluate_expresion(expr: String) {
+    fn eval_arithmetic(operand: &str, expr: &String) -> String {
+        let args :Vec<f32> = parsing::parse_args(expr)
+            .iter()
+            .map(|x| { x.parse::<f32>().unwrap() })
+            .collect();
 
+        let mut temp_res : f32 = 0.0;
+
+        match operand {
+            "+" => { temp_res = args.into_iter().reduce(|a, b|  a + b ).unwrap() }
+            "*" => { temp_res = args.into_iter().reduce(|a, b|  a * b ).unwrap() }
+            "/" => { temp_res = args.into_iter().reduce(|a, b|  a / b ).unwrap() }
+            "-" => { temp_res = args.into_iter().reduce(|a, b|  a - b ).unwrap() }
+            _ => {}
+        }
+
+
+        temp_res.to_string()
+    }
+
+    fn eval_boolean(operand: &str, expr: &String) -> String {
+        let args = parsing::parse_args(expr);
+
+
+        String::from("")
+    }
+
+    fn eval_conditionals(expr: &String) -> String {
+
+
+        String::from("")
     }
 }
 
@@ -40,6 +144,8 @@ mod parsing {
     static OPEN_EXPR : char = '(';
     static CLOSE_EXPR : char = ')';
 
+    /// heuristic to see in which order things are going to be evaluated in
+    /// basically just the matching parenthesis algorithm
     pub fn expression_order(stg: &String) -> Vec<[usize; 2]> {
         
 
@@ -74,6 +180,10 @@ mod parsing {
 
     }
 
+
+    /// given a string and the location of an opening parenthesis
+    /// it will find the location in the string of the matching closing parenthesis
+    /// returns 0 if it can find it
     pub fn find_matching_parenthesis(stg: &String, begin: usize) -> usize {
         let mut num_open = 0;
 
@@ -89,9 +199,18 @@ mod parsing {
                 num_open -= 1;
             }
         }
-        begin
+        0
     }
 
-
+    /// NOT YET IMPLEMENTED
+    /// given a string in the form of 
+    /// operand arg arg arg...
+    /// it will give the operands in a vector form
+    /// note that operands can be other expressions
+    /// EX: + (+ 1 1) (* 4 5) (/ 15 (+ 2 1))
+    /// would return {(+ 1 1), (* 4 5), (/ 15 (+ 2 1))}
+    pub fn parse_args(stg: &String) -> Vec<String> {
+        Vec::new()
+    }
 
 }
