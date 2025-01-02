56efc8ed937b6ff89cf9748783b1a04a95ca4467
wataflai <=>
2025-01-02 12:24:38 -0600

    made sample programs harder

--- sample_progs/conditionals.rkt
+++ sample_progs/conditionals.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
+(if #t 1 3)
 (if (< 5 3) 5 (* 3 2))
+(or #t #t)
+(xor #t #f)
+(and #t #t)
+(nand #t #t)
+
+(if (or #t #t #f #f #f #t)  (nand #t #t (= 4 3) #f #t (< (* 2 3) 9)) (xor #t #t))
